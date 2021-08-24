357fabed8b2d43530321dfcb01f55ea478043314
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-08-24 19:14:59 +0200

    fix ambiguous variables

--- repetitions/repetitions.py
+++ repetitions/repetitions.py
@@ -3,7 +3,7 @@ import csv
 
 with open("../enrich/enriched.csv") as f:
     for row in csv.reader(f):
-        puzzle_id, fen, moves, rating, deviation, popularity, played, themes, url, moves = row
+        puzzle_id, fen, puzzle_moves, rating, deviation, popularity, played, themes, url, game_moves = row
 
         game_and_ply = url.removeprefix("https://lichess.org/")
         game, ply = game_and_ply.split("#")
@@ -11,7 +11,7 @@ with open("../enrich/enriched.csv") as f:
         ply = int(ply)
 
         board = chess.Board()
-        for uci in moves.split()[:ply]:
+        for uci in game_moves.split()[:ply]:
             board.push_uci(uci)
 
         while board.move_stack:
