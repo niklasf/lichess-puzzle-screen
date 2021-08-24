44484816157cdfec63d591a2f7c5480a808a666c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-08-24 16:46:14 +0200

    start parsing moves

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,76 @@
 use std::env;
 use std::io;
 use std::fs::File;
+use std::str;
 
-use pgn_reader::{BufferedReader, Visitor};
+use pgn_reader::{BufferedReader, Visitor, RawHeader, Skip};
+use shakmaty::{Chess, Position as _ };
+use shakmaty::san::{SanPlus};
+use shakmaty::uci::Uci;
+use skytable::Connection;
 
-struct Importer;
+struct Importer {
+    con: Connection,
+    pos: Chess,
+    id: Option<String>,
+    moves: Vec<Uci>,
+}
+
+impl Importer {
+    fn new() -> io::Result<Importer> {
+        Ok(Importer {
+            con: Connection::new("127.0.0.1", 2003)?,
+            id: None,
+            pos: Chess::default(),
+            moves: Vec::new(),
+        })
+    }
+}
 
 impl Visitor for Importer {
     type Result = ();
 
-    fn end_game(&mut self) { }
+    fn begin_game(&mut self) {
+        self.pos = Chess::default();
+        self.moves.clear();
+    }
+
+    fn header(&mut self, key: &[u8], value: RawHeader<'_>) {
+        if key == b"Variant" {
+            panic!("unexpected variant game")
+        } else if key == b"FEN" {
+            panic!("unexpected game from position")
+        } else if key == b"Site" {
+            self.id = str::from_utf8(value.as_bytes())
+                .unwrap()
+                .strip_prefix("https://lichess.org/")
+                .map(|s| s.to_owned());
+        }
+    }
+
+    fn begin_variation(&mut self) -> Skip {
+        Skip(true)
+    }
+
+    fn san(&mut self, san_plus: SanPlus) {
+        if self.id.is_some() {
+            if let Ok(m) = san_plus.san.to_move(&self.pos) {
+                self.pos.play_unchecked(&m);
+            } else {
+                self.id = None;
+            }
+        }
+    }
+
+    fn end_game(&mut self) {
+        if let Some(id) = self.id.take() {
+            println!("{} {:?}", id, self.moves);
+        }
+    }
 }
 
-fn main() -> Result<(), io::Error> {
-    let mut importer = Importer;
+fn main() -> io::Result<()> {
+    let mut importer = Importer::new()?;
 
     for arg in env::args().skip(1) {
         let file = File::open(&arg)?;
